1681cbd1b0cd692ea32cab1eda3fb85feb30995b
romaska877 <iliciuctiberius@gmail.com>
2026-05-14 15:32:28 +0100

    Add files via upload
    Add FAQ accordion section

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ navItems.forEach(function (link) {
     navLinks.classList.remove("active");
   });
 });
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(function (item) {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", function () {
+    faqItems.forEach(function (otherItem) {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
+    });
+
+    item.classList.toggle("active");
+  });
+});
